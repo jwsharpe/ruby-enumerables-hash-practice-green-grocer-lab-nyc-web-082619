cb991e5fd5238bd9291071fb11e5c99b2c6a9d6a
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-04 21:37:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,18 +1,18 @@
 def consolidate_cart(cart)
-  consolidated_cart = []
+  consolidated_cart = {}
   for item in cart do
     item_key = item.keys[0]
-	  item_values = item.values[0]
-	  
-  	if !consolidated_cart[item_key]
+	item_values = item.values[0]
+	if (!consolidated_cart[item_key])
       item_values[:count] = 1
-  	  consolidated_cart[item_key] = item_values
-    	else
-  	  consolidated_cart[item_key][:count] += 1
-    end
+	  consolidated_cart[item_key] = item_values
+  	else
+	  consolidated_cart[item_key][:count] += 1
+  	end
   end
   return consolidated_cart
 end
+
 
 def apply_coupons(cart, coupons)
   coupon_cart = cart
